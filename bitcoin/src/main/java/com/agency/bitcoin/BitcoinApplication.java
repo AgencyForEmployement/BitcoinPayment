f1ce9d18b05b0efbe7abe27a519a3b6be06d8569
mihailomajstorovic47 <73445435+mihailomajstorovic47@users.noreply.github.com>
2023-01-31 16:22:15 +0100

    bitcoin service added

--- bitcoin/src/main/java/com/agency/bitcoin/BitcoinApplication.java
+++ bitcoin/src/main/java/com/agency/bitcoin/BitcoinApplication.java
@@ -2,6 +2,8 @@ package com.agency.bitcoin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BitcoinApplication {
@@ -10,4 +12,10 @@ public class BitcoinApplication {
 		SpringApplication.run(BitcoinApplication.class, args);
 	}
 
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
+
+
